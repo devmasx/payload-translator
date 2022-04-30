fe434ea54f135c3df2e1f2c10ae2896c50389d58
Miguel Savignano <migue.masx@gmail.com>
2022-05-01 01:26:42 +0200

    test deepp object

--- lib/payload_translator/field_resolver.rb
+++ lib/payload_translator/field_resolver.rb
@@ -1,46 +1,48 @@
-class PayloadTranslator::FieldResolver
-  attr_reader :config
-  def initialize(field_config)
-    @config = field_config
-  end
-
-  def resolve(payload)
-    if deep_object?
-      resolve_deep_object(payload)
-    elsif config["$fnc"]
-      resolve_fnc(payload)
-    elsif config["$map"]
-      resolve_map(payload)
-    else
-      resolve_value(payload)
+module PayloadTranslator
+  class FieldResolver
+    attr_reader :config
+    def initialize(field_config)
+      @config = field_config
     end
-  end
 
-  def resolve_value(payload)
-    payload[config["$field"]]
-  end
-
-  def resolve_fnc(payload)
-    config["$fnc"]
-    handlers = PayloadTranslator.configuration.handlers
-    handler = handlers.fetch(config["$fnc"].to_sym)
-    handler.call(payload)
-  end
-
-  def resolve_map(payload)
-    value = payload[config["$field"]]
-    return config["$default"] unless value
-
-    config["$map"].fetch(value) { config["$map_default"] }
-  end
-
-  def resolve_deep_object(payload)
-    config.each_with_object({}) do |(target_name, field_config), result|
-      result[target_name] = FieldResolver.new(field_config).resolve(payload)
+    def resolve(payload)
+      if deep_object?
+        resolve_deep_object(payload)
+      elsif config["$fnc"]
+        resolve_fnc(payload)
+      elsif config["$map"]
+        resolve_map(payload)
+      else
+        resolve_value(payload)
+      end
     end
-  end
 
-  def deep_object?
-    !config.keys.any? {|key| key =~ /\$/ }
+    def resolve_value(payload)
+      payload[config["$field"]]
+    end
+
+    def resolve_fnc(payload)
+      config["$fnc"]
+      handlers = PayloadTranslator.configuration.handlers
+      handler = handlers.fetch(config["$fnc"].to_sym)
+      handler.call(payload)
+    end
+
+    def resolve_map(payload)
+      value = payload[config["$field"]]
+      return config["$default"] unless value
+
+      config["$map"].fetch(value) { config["$map_default"] }
+    end
+
+    def resolve_deep_object(payload)
+      config.each_with_object({}) do |(target_name, field_config), result|
+        result[target_name] = FieldResolver.new(field_config).resolve(payload)
+      end
+    end
+
+    def deep_object?
+      !config.keys.any? {|key| key =~ /\$/ }
+    end
   end
 end
--- spec/payload_translator_spec.rb
+++ spec/payload_translator_spec.rb
@@ -4,19 +4,18 @@ require 'json'
 
 describe PayloadTranslator::Service do
   let(:subject) { PayloadTranslator::Service.new(config) }
-
-  let(:config) { YAML.load_file './spec/fixtures/with_map/config.yaml' }
-  let(:input) { JSON.parse File.read('./spec/fixtures/with_map/input.json')}
+  let(:config) { YAML.load_file "./spec/fixtures/#{context}/config.yaml" }
+  let(:input) { JSON.parse File.read("./spec/fixtures/#{context}/input.json")}
 
   context "with $map" do
+    let(:context) { "with_map"}
     it '#translate' do
       expect(subject.translate(input)).to eq({"id"=>"1", "login_type"=>"APP", "user_name"=>"Jhon Doe"})
     end
   end
 
   context "with $fnc" do
-    let(:config) { YAML.load_file './spec/fixtures/with_fnc/config.yaml' }
-    let(:input) { JSON.parse File.read('./spec/fixtures/with_fnc/input.json')}
+    let(:context) { "with_fnc" }
 
     let(:subject) {
       PayloadTranslator.configure do |config|
@@ -24,12 +23,28 @@ describe PayloadTranslator::Service do
           get_name: ->(payload) { payload['name'] }
         }
       end
-
       PayloadTranslator::Service.new(config)
     }
 
     it '#translate' do
        expect(subject.translate(input)).to eq({"user_name"=>"Jhon Doe"})
+    end
+  end
+
+  context "with depp object" do
+    let(:context) { "with_deep_object" }
+
+    let(:subject) {
+      PayloadTranslator.configure do |config|
+        config.handlers = {
+          get_name: ->(payload) { payload['name'] }
+        }
+      end
+      PayloadTranslator::Service.new(config)
+    }
+
+    it '#translate' do
+       expect(subject.translate(input)).to eq({"user" => {"login"=>{"type"=>"APP"}, "name"=>"Jhon Doe"}})
     end
   end
 end
