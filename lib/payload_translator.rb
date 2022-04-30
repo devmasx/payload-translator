a300758a21d3669c41f8c6248c002cce63947785
Miguel Savignano <migue.masx@gmail.com>
2022-05-01 01:10:17 +0200

    allow handlers configuration

--- lib/payload_translator.rb
+++ lib/payload_translator.rb
@@ -4,5 +4,25 @@ loader = Zeitwerk::Loader.for_gem
 loader.setup
 
 module PayloadTranslator
+  class Config
+    def initialize
+      @handlers = {}
+    end
+
+    def handlers
+      @handlers
+    end
+
+    def handlers=(value)
+      @handlers.merge!(value)
+    end
+  end
+
+  def self.configure
+    @@config ||= Config.new
+    yield(@@config)
+    @@config
+  end
+
   VERSION = "0.0.1"
 end
--- spec/payload_translator_spec.rb
+++ spec/payload_translator_spec.rb
@@ -3,14 +3,33 @@ require 'yaml'
 require 'json'
 
 describe PayloadTranslator::Service do
+  let(:subject) { PayloadTranslator::Service.new(config) }
+
+  let(:config) { YAML.load_file './spec/fixtures/with_map/config.yaml' }
+  let(:input) { JSON.parse File.read('./spec/fixtures/with_map/input.json')}
+
   context "with $map" do
-    let(:config) { YAML.load_file './spec/fixtures/with_map/config.yaml' }
-    let(:input) { JSON.parse File.read('./spec/fixtures/with_map/input.json')}
-
-    let(:subject) { PayloadTranslator::Service.new(config) }
-
-    it 'translate' do
+    it '#translate' do
       expect(subject.translate(input)).to eq({"id"=>"1", "login_type"=>"APP", "user_name"=>"Jhon Doe"})
+    end
+  end
+
+  context "with $fnc" do
+    let(:config) { YAML.load_file './spec/fixtures/with_fnc/config.yaml' }
+    let(:input) { JSON.parse File.read('./spec/fixtures/with_fnc/input.json')}
+
+    let(:subject) {
+      PayloadTranslator.configure do |config|
+        config.handlers = {
+          get_name: ->(payload) { payload['name'] }
+        }
+      end
+
+      PayloadTranslator::Service.new(config)
+    }
+
+    it '#translate' do
+       expect(subject.translate(input)).to eq({"user_name"=>"Jhon Doe"})
     end
   end
 end
