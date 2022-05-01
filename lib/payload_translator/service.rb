8d6e431b58eca5e09a0d99c6e8b4dc7a68f64460
Miguel Savignano <migue.masx@gmail.com>
2022-05-01 02:20:02 +0200

    merge global and service handlers & formatters (#3)
    * merge global and service handlers & formatters
    
    * README

--- lib/payload_translator/service.rb
+++ lib/payload_translator/service.rb
@@ -1,14 +1,22 @@
 module PayloadTranslator
   class Service
-    attr_reader :config
+    attr_reader :adapt_config, :configuration
 
-    def initialize(config)
-      @config = config
+    def initialize(adapt_config, handlers: {}, formatters: {})
+      @adapt_config = adapt_config
+      @configuration = merge_configuration(handlers: handlers, formatters: formatters)
+    end
+
+    def merge_configuration(handlers: , formatters:)
+      PayloadTranslator::Config.new.tap do |config|
+        config.handlers = PayloadTranslator.configuration.handlers.merge(handlers)
+        config.formatters = PayloadTranslator.configuration.formatters.merge(formatters)
+      end
     end
 
     def translate(payload)
-      config["payload"].each_with_object({}) do |(target_name, field_config), result|
-        result[target_name] = FieldResolver.new(field_config).resolve(payload)
+      adapt_config["payload"].each_with_object({}) do |(target_name, field_config), result|
+        result[target_name] = FieldResolver.new(field_config, configuration).resolve(payload)
       end
     end
   end
